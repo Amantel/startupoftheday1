5f8ce3b63318d2651956832ed9ec42fdb3cba1e9
amantel <amantels@gmail.com>
2018-10-06 12:22:09 +0300

    Fixed bad loading

--- src/containers/App.js
+++ src/containers/App.js
@@ -44,21 +44,26 @@ class App extends Component {
     componentWillMount() {
         this.props.dispatch(vkActions.initApp());
 
-        this.props.dispatch(startupActions.loadUser());
         let user =  JSON.parse(localStorage.getItem('startupOfTheDayUser'))
+        this.props.dispatch(startupActions.loadUser());
         if(!this.props.articles) {
           if(!user)
           {
               this.props.dispatch(startupActions.fetchArticles());
           } else {
-            const lastVisit = new Date(user.lastVisit).getTime()/1000/60/60; //hours
-            const timeNow = new Date().getTime()/1000/60/60; //hours
-            const hasChanged = timeNow - lastVisit > 3; //more than 3 hours
+
+            const lastVisit = new Date(user.lastVisit).getTime()/1000/60; //hours
+            const timeNow = new Date().getTime()/1000/60; //hours
+            const hasChanged = timeNow - lastVisit > 2; //more than 3 hours
+
 
             let storedArticles = [];
             if(localStorage.getItem('startupOfTheDayArticles')!=null) {
               storedArticles = JSON.parse(localStorage.getItem('startupOfTheDayArticles'));
             }
+
+
+
 
             if(!hasChanged && storedArticles.length!==0) {
               this.props.dispatch(startupActions.fetchArticlesFromMemory());
