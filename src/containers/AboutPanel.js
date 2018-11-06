ac64291dcadd6e2a6693e7da955795dab53db7ef
amantel <amantels@gmail.com>
2018-11-06 15:04:50 +0300

    More "other places to read"

--- src/containers/AboutPanel.js
+++ src/containers/AboutPanel.js
@@ -20,15 +20,33 @@ class AboutPanel extends Component {
                 <AboutContent/>
 
                 <UI.Group title="Этот блог на других площадках">
-                          <UI.Link href="https://t.me/startupoftheday/"><UI.CellButton>
-                            Telegram
-                          </UI.CellButton>
-                          </UI.Link>
-                          <UI.Link href="https://www.facebook.com/gornal/">
-                          <UI.CellButton className="topGreyBorder">
-                            Facebook
-                          </UI.CellButton>
+                            <UI.Link target="_blank" href="https://t.me/startupoftheday/"><UI.CellButton>
+                              Telegram
+                            </UI.CellButton>
                             </UI.Link>
+                            <UI.Link target="_blank" href="https://www.facebook.com/gornal/">
+                              <UI.CellButton className="topGreyBorder">
+                                Facebook
+                              </UI.CellButton>
+                            </UI.Link>
+                            <UI.Link target="_blank" href="https://icq.com/chat/startups">
+                              <UI.CellButton className="topGreyBorder">
+                                ICQ
+                              </UI.CellButton>
+                            </UI.Link>
+                            <UI.Link target="_blank" href="https://vk.com/startupoftheday">
+                              <UI.CellButton className="topGreyBorder">
+                                Группа Вконтакте
+                              </UI.CellButton>
+                            </UI.Link>
+                            <UI.Link target="_blank" href="http://startupoftheday.ru">
+                              <UI.CellButton className="topGreyBorder">
+                                startupoftheday.ru
+                              </UI.CellButton>
+                            </UI.Link>
+
+
+
                 </UI.Group>
             </UI.Panel>
         );
